eeb515f25fbf4fa650d73f66fb3197a9a5f9d529
antival3nt <xxzupxx@gmail.com>
2022-05-26 08:47:58 +0200

    added 10k times search and timers

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Binary search script
 
+import random
+import time
+
 # naive search: scan entire list and ask if it's equal to the target
 # if yes, return the index
 # if no, return -1
+from random import Random, random
+
+
 def naive_search(l, target):
     # Example l = [1, 3, 10, 12]
     for i in range(len(l)):
@@ -30,3 +36,30 @@ def binary_search(l, target, low=None, high=None):
         return binary_search(l, low, midpoint-1)
     else:
         return binary_search(l, target, midpoint+1, high)
+
+if __name__ == "__main__":
+    # l = [1, 3, 5, 10, 12]
+    # target = 10
+    # print(naive_search(l, target))
+    # print(binary_search(l, target))
+
+    length = 10000
+    # Build sorted list of 10k
+    sorted_list = set()
+    while len(sorted_list) < length:
+        sorted_list.add(Random.randint(-3 * length, 3 * length))
+    sorted_list = list(sorted_list)
+
+    # Run naive search on sorted list 10.000 times
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print("Naive search: ", end - start/length, "seconds")
+
+    # Run binary search on sorted list 10.000 times
+    start = time.time()
+    for target in sorted_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print("Binary search: ", end - start/length, "seconds")
